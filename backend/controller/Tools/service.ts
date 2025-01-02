49108e858056c6a03eb86a00ea016b98988f55c7
Alex CANDAS <candas.alex@gmail.com>
2025-01-02 12:42:36 +0100

    migration bdd

--- backend/controller/Tools/service.ts
+++ backend/controller/Tools/service.ts
@@ -1,6 +1,6 @@
 
 
-import { Ingredient,Ingredient2, isIngredient, isRecipe, UnitEnum } from "../../shared/back.model";
+import { Ingredient,Ingredient2, Recipe2, UnitEnum } from "../../shared/back.model";
 import { getBDD, saveBDD } from "../../lib/utils";
 
 
@@ -13,13 +13,47 @@ export const testToolsSv = async () => {
 
 export const migrationToolsSv = async () => {
 
-
   console.log("migrationToolsSv");
 
   let bdd = getBDD();
 
-  bdd.ingredientstmp.length = 0;    //suppression de toutes les données
+  //suppression de toutes les données tmp qui restent éventuellement des anciennes migrations
+  bdd.ingredientstmp.length = 0; 
+  bdd.recipestmp.length = 0; 
 
+  
+  bdd.ingredients.forEach((ing_old) => {
+    const ing_new:Ingredient2 = {
+      id: ing_old.id,
+      idUser: 1,
+      unit: UnitEnum.GRAM,           
+      name: ing_old.name,
+      description: ing_old.description, 
+      price: ing_old.price,       
+    };
+    bdd.ingredientstmp.push(ing_new);
+    console.log(ing_old);
+    console.log(ing_new);
+  });
+
+
+  bdd.recipes.forEach((rec_old) => {
+    const rec_new:Recipe2 = {
+      id: rec_old.id,
+      idUser: 1,
+      test: false,
+      ingredient: false,
+      commentaire: '',
+      name: rec_old.name,
+      description: rec_old.description,
+      steps: rec_old.steps,
+      ingredientsQte: rec_old.ingredientsQte,   
+    };
+
+    bdd.recipestmp.push(rec_new);
+    console.log(rec_old);
+    console.log(rec_new);
+  });
 
 
 

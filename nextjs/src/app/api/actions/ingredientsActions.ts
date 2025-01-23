4ba18c27bab781624bd1c5039ae923597970a6ef
Alex CANDAS <candas.alex@gmail.com>
2025-01-23 16:45:44 +0100

    .

--- nextjs/src/app/api/actions/ingredientsActions.ts
+++ nextjs/src/app/api/actions/ingredientsActions.ts
@@ -8,35 +8,17 @@ import { saveBDD } from "@/utils/utils";
 import recipeData from '@/utils/bdd.json';
 import { Bdd } from "@/models/models";
 
-
 export async function getIngredientsUser(input: { p_idUser: number }) {
         let usrIngredient:Ingredient[] = recipeData.ingredients; 
         usrIngredient = usrIngredient.filter(ing => ing.idUser == input.p_idUser)
         return usrIngredient
   }
 
-
-
-
-
 //export async function getAllIngredientsTEST(input: { p_idUser: number }) {
 export async function getAllIngredientsTEST() {
     const bdd:Bdd = getBDD();
     return bdd.ingredients;
-/*
-    let usrIngredient:Ingredient[] = 
-    usrIngredient = usrIngredient.filter(ing => ing.idUser == input.p_idUser)
-    return usrIngredient
-
-*/
 }
-
-
-
-
-
-
-
 
 export async function addIngredient(p_ing: Ingredient) {
 
@@ -51,3 +33,11 @@ export async function addIngredient(p_ing: Ingredient) {
     saveBDD(bdd);                              
     return p_ing;
 }
+
+
+
+
+
+
+
+

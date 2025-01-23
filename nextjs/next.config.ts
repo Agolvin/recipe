8d42fb581accc7260ae2088de312bcff8803d0ac
Alex CANDAS <candas.alex@gmail.com>
2025-01-23 14:20:05 +0100

    .

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  
   /* config options here */
+
+  /*
+  experimental: {
+    serverActions: true, // Activation de serverActions
+  },
+  */
+
+  experimental: {
+    serverActions: {}, // Activation de serverActions
+  },
+
 };
 
 export default nextConfig;
